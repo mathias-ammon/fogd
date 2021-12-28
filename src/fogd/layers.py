85b71cdd7537fd42e09fb7e9a296cf82c9b5a564
tZE <WriteMe@NoEmail.net>
2021-12-28 15:21:56 +0100

    Add codestyle revs to latest dependency update

--- src/fogd/layers.py
+++ src/fogd/layers.py
@@ -1,3 +1,5 @@
-"""
-Module handling the different forest garden layer archetypes.
+"""Module handling the different forest garden layer archetypes.
+
+Even though used for temperate forest gardens, this tools assumes 7 layers
+instead of the common 3 layers.
 """
